d8adf5c0144701976efa78c6dcd77279bc95c318
RadSan7 <radek.san7@icloud.com>
2025-07-13 01:27:19 +0200

    Asteroid Field

---
+++ asteroid.py
@@ -0,0 +1,18 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", ((self.position.x), (self.position.y)), self.radius, 2)
+
+    def update(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += self.velocity * dt
+
+    
+        
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import pygame
 from constants import *
 from player import Player
+from AsteroidField import *
+from circleshape import CircleShape
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    AsteroidField.containers = (updatable,)
+    Player.containers = (drawable, updatable)
+    Asteroid.containers = (asteroids, drawable, updatable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    asteroid_field = AsteroidField()
     dt = 0
 
     while True:
@@ -15,17 +27,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        updatable.update(dt)
+        for asteroid in asteroids:
+            if CircleShape.collide(player, asteroid) == False:
+                pygame.quit()
+                print("Game Over")
+                main()
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
-        pygame.display.flip()
 
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
-    print("Starting Asteroids!")
-
-
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
