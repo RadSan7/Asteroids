3baf4f3079aebbb511818109f9513f0df2262eaf
RadSan7 <radek.san7@icloud.com>
2025-07-12 21:06:59 +0200

    done

--- main.py
+++ main.py
@@ -4,13 +4,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Hello from asteroids!")
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill((0, 0, 0))    
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
